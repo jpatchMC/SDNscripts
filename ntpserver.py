2a99339235f7aefcbe443d74f14009696bb79b26
Josh Patch <jpatch@madisoncollege.edu>
2023-01-28 12:20:42 -0600

    week 3 part 2 and 3, i think

--- ntpserver.py
+++ ntpserver.py
@@ -4,15 +4,22 @@ def main():
     "Server2" : "201.0.113.22",
     "Server3" : "58.23.191.6"
     }
-    IPs =[]
-    print(f"Server \t\t Address")
-    print("-"*50)
-    for item in ntpserver.items():
-        print(f"{item[0]} \t {item[1]}")
+    IPs = []
+    report(ntpserver,IPs)
         
-        IPs.append(item[1])
+        
     #print (IPs)
     PingPrep(IPs)
+def report(wut,IPs):
+
+     #could be a problem later
+    print(f"Server \t\t Address")
+    print("-"*50)
+    for item in wut.items():
+        print(f"{item[0]} \t {item[1]}")
+        IPs.append(item[1])
+        #return IPs
+
 def PingPrep(ipList):
     for addy in ipList:
         print(f"Ping {addy}")
