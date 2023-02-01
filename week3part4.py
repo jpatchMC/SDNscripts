9ff4276e9d76dd3ddaa5d4d9dae9fd02ad92cbf0
Josh Patch <jpatch@madisoncollege.edu>
2023-01-31 18:44:40 -0600

    changed the while loops to be less brittle

--- week3part4.py
+++ week3part4.py
@@ -1,4 +1,4 @@
-#josh patch
+#josh patch a little chatGTP
 devices = {"R1": {"type" : "router","hostname" : "R1","mgmtIP" : "10.0.0.1"},"R2":{"type" : "router","hostname" : "R2","mgmtIP" : "10.0.0.2"},"S1":{"type" : "switch","hostname" : "S1","mgmtIP" : "10.0.0.3"},"S2":{"type" : "switch","hostname" : "S2","mgmtIP" : "10.0.0.4"}}
 
 #print(devices)
