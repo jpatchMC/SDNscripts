23b118e9569690e428644ee7e96d79486855611b
Josh Patch <jpatch@madisoncollege.edu>
2023-04-15 13:04:14 -0500

    week 12 done

--- week12/unit11 Lab.py
+++ week12/unit11 Lab.py
@@ -42,6 +42,7 @@ def denny():
 
 #print("I" * 40)
 def show_ip_int_yang(interfaces):
+    #take in dict and makes a table
     print("Name\t\t\tip\t\t\tsubnet")
     for interface in interfaces:
         #print(interface)
@@ -53,6 +54,7 @@ def show_ip_int_yang(interfaces):
         
             print(interface['name']+"\t"+ip+ "\t"+sub)
 def new_ip(ip_ask):
+    #checks to make sure input is formated correctly for an ip address
     check_number =ip_ask.replace(".","")
     if check_number.isnumeric(): 
         IP_check = ip_ask.split(".") #i need to check them individually
@@ -75,33 +77,36 @@ def new_ip(ip_ask):
         valid_bool = False
     return valid_bool
 
-def modify_interfaces_whichINT(interface,intnamechange):
-    for item in interface:
-            if item == intnamechange:
-                
-                if intnamechange != "GigabitEthernet1":
-                    #print("IF YOU MODIFY THAT INTERFACE WE'RE GONNA HAVE A BAD TIME")
-                    validbool = True
-                else:
-                    #print("yay")
-                    validbool = False
-                #break#sorry denny couldn't get it to not print invalid interface w/out this
-    #else:
-                #print("Inavlaid Interface\n")
-                #validbool = False
-    #print(intname)
+def modify_interfaces_whichINT(validbool,interfaces,intnamechange):
+    # takes interface dict and input from main checks to make sure they arn't ints we don't want to mod and then also check to make sure its a valid interface in kind of a goofy order 
+    if intnamechange == "GigabitEthernet1" or intnamechange == "Loopback0":
+        print("IF YOU MODIFY THAT INTERFACE WE'RE GONNA HAVE A BAD TIME")
+        validbool = False
+    else:
+        for interface in interfaces:
+            if interface['name'] == intnamechange:
+                #print("yay")
+                validbool = True            
+            
     return validbool
+
+def ins_new_info(interfaces,intname_change,ip_change,sub_change):
+    #changes information in original dict with new values based on what int we've already defined to change and out new ip and subnet
+    for interface in interfaces:
+        if interface['name'] == intname_change:
+            interface['ipv4']['address']['ip']=ip_change
+            interface['ipv4']['address']['netmask']=sub_change
+    return interfaces
+
 
 def main():
     interfaces = denny()
+    print(interfaces)
     show_ip_int_yang(interfaces)
     valBool_intchange= False
     while valBool_intchange == False:
-       intname_change = input("what interface would you like to change?\n")
-       for interface in interfaces:
-        if interface['name'] != 'Loopback0':
-            current_int = interface['name']
-            valBool_intchange = modify_interfaces_whichINT(current_int,intname_change)
+        intname_change = input("what interface would you like to change?\n")#"GigabitEthernet2"
+        valBool_intchange = modify_interfaces_whichINT(valBool_intchange,interfaces,intname_change)
     valBool_IP = False
     while valBool_IP == False:
         ip_change= input("new ip?\n")
@@ -110,7 +115,12 @@ def main():
     while valbool_sub == False:
         sub_change = input("and the new subnet")
         valbool_sub = new_ip(sub_change)
-    print(intname_change,ip_change,sub_change)
+    #print(intname_change,ip_change,sub_change)
+    interfaces = ins_new_info(interfaces,intname_change,ip_change,sub_change)
+    show_ip_int_yang(interfaces)
+
+
+
 
 if __name__== "__main__" :
     main()
